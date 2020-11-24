eec168d2d7cf35245a13909416a46f7efadd0e37
swagere <1250539180@qq.com>
2020-11-24 21:45:07 +0800

    login and register page

--- src/main/java/com/kve/ems/EmsApplication.java
+++ src/main/java/com/kve/ems/EmsApplication.java
@@ -3,12 +3,22 @@ package com.kve.ems;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 @MapperScan("com.kev.dao") //扫描所有dao接口
-public class EmsApplication {
+public class EmsApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(EmsApplication.class, args);
+    }
+
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //引入静态资源css、js无效时加上
+//        registry.addResourceHandler("/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        super.addResourceHandlers(registry);
     }
 }
---
+++ src/main/java/com/kve/ems/controller/indexController.java
@@ -0,0 +1,18 @@
+package com.kve.ems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/index")
+    public String toIndex(){
+        return "login";
+    }
+
+    @GetMapping("/toRegister")
+    public String toRegister(){
+        return "register";
+    }
+}
