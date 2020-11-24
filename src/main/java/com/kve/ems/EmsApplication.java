ba382cafd57652592dbdba5f368986526faf1f84
swagere <1250539180@qq.com>
2020-11-24 18:54:23 +0800

    Project construction build

--- src/main/java/com/kve/ems/EmsApplication.java
+++ src/main/java/com/kve/ems/EmsApplication.java
@@ -1,13 +1,14 @@
 package com.kve.ems;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kev.dao") //扫描所有dao接口
 public class EmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmsApplication.class, args);
     }
-
 }
